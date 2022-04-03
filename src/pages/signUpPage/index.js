7c7faee785fa752aed2d13c962ea5168ce9a6a52
ShenQi <1017706039@qq.com>
2022-04-03 14:33:32 +0800

    fix

--- src/pages/signUpPage/index.js
+++ src/pages/signUpPage/index.js
@@ -7,6 +7,7 @@ import * as flashActions from "../../actions/flashMessageActions";
 
 class SignupPage extends React.Component {
     render() {
+    
         return (
             <div>
                 <SignupForm flashActions={this.props.flashActions} signupActions={this.props.signupActions} />
